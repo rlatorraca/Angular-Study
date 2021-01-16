5fe52930b4b147a12737d27e9bebe5687206ab05
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-01-16 00:18:43 -0400

    Added examples of directives: ngIf, ngFor, ngSwitch, ngClass and ngStyle

--- Angular-Diretivas/src/app/app.module.ts
+++ Angular-Diretivas/src/app/app.module.ts
@@ -1,20 +1,28 @@
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+
+import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    DiretivaNgswitchComponent
+    DiretivaNgswitchComponent,
+    DiretivaNgclassComponent,
+    DiretivaNgstyleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
