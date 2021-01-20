a9331409c6d493ad0417671ef938601c221b76ed
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-01-19 22:43:44 -0400

    Added examples of singletons and instance variables

--- Angular-Servicos/src/app/app.module.ts
+++ Angular-Servicos/src/app/app.module.ts
@@ -1,22 +1,26 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
+import { CursosModule } from './cursos/cursos.module';
 
 import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule} from './criar-curso/criar-curso.module'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent
+    AppComponent
+        
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [CursosService], // Servicos sao declarado aqui, ficando disponivel para toda aplicaca pois esta em app.modules.ts
+  providers: [CursosService], // Servicos sao declarado aqui, ficando disponivel (a instanciacao) para toda aplicaca pois esta em app.modules.ts
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Angular-Servicos/src/app/cursos/cursos.service.ts
+++ Angular-Servicos/src/app/cursos/cursos.service.ts
@@ -2,7 +2,19 @@ import { Injectable } from "@angular/core";
 
 @Injectable() // Permite a Injeccao de Dependencia em outra classe que necessite
 export class CursosService {
+
+    private cursos: string[] = ['Java 14', 'Quarkus', "Angular2 v.11"];
+
+
+    constructor(){
+        console.log("Construtor da CursoService")
+    }
+
     getCursos() {
-        return ['Java 14', 'Quarkus', "Angular2 v.11"];
+        return this.cursos;
+    }
+
+    addCurso(curso: string){
+        this.cursos.push(curso);
     }
 }
