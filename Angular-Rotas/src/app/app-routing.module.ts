c46e545629ee1359bbe61f781a2be9eeef4f831d
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-01-22 22:28:05 -0400

    Addes Simples routes examples

--- Angular-Rotas/src/app/app-routing.module.ts
+++ Angular-Rotas/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'cursos', component: CursosComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], // Conta as rotas de toda aplicacao
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
