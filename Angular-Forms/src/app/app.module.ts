567390146b36df69168a3b334f450088c9a9ecaf
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-02-02 00:44:01 -0400

    Added values initialization on template drive example; Added error messages on form

--- Angular-Forms/src/app/app.module.ts
+++ Angular-Forms/src/app/app.module.ts
@@ -1,22 +1,26 @@
+import { TemplateFormModule } from './template-form/template-form.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TemplateFormComponent } from './template-form/template-form.component';
+//import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TemplateFormComponent,
+    AppComponent,    
+    //TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular-Forms/src/app/template-form/template-form.component.ts
+++ Angular-Forms/src/app/template-form/template-form.component.ts
@@ -6,14 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent implements OnInit {
+  
+  usuario: any = {
+    nome: "Rodrigo",
+    email: "rodrigo@email.com.ca"
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onSubmit() {
-    
+  onSubmit(form) {
+    console.log(this.usuario);
   }
 
 }
