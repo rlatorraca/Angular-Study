9a93b74846c9f126aab813112351ccb90faf8da3
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-02-03 00:34:36 -0400

    Added a code to cjeck the button submit on the form; Added a component to verify form data; added fields on the form (first version)

--- Angular-Forms/src/app/template-form/template-form.module.ts
+++ Angular-Forms/src/app/template-form/template-form.module.ts
@@ -1,18 +1,19 @@
+import { FormDebugComponent } from './../form-debug/form-debug.component';
 import { TemplateFormComponent } from './template-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 
-
 @NgModule({
   declarations: [
     TemplateFormComponent,
-    
+    FormDebugComponent,
   ],
   imports: [    
     CommonModule,
-    FormsModule
+    FormsModule,
+    
   ]
 })
 export class TemplateFormModule { }
