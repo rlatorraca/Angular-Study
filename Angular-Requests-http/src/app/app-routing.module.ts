a8c38d9c555f2e94ba706beefb6b7d1dad84675c
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-05-02 23:20:23 -0300

    New routing way updated

--- Angular-Requests-http/src/app/app-routing.module.ts
+++ Angular-Requests-http/src/app/app-routing.module.ts
@@ -5,9 +5,21 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'upload' },
-  { path: 'courses', loadChildren: './courses/courses.module#CoursesModule' },
-  { path: 'upload',  loadChildren: './upload-file/upload-file.module#UploadFileModule'},
-  { path: 'rxjs-poc', loadChildren: './unsubscribe-rxjs/unsubscribe-rxjs.module#UnsubscribeRxjsModule' }
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+    //loadChildren: './courses/courses.module#CoursesModule' 
+  },
+  {
+    path: 'upload',
+    loadChildren: () => import('./upload-file/upload-file.module').then(m => m.UploadFileModule)
+    //loadChildren: './upload-file/upload-file.module#UploadFileModule'
+  },
+  {
+    path: 'rxjs-poc',
+    loadChildren: () => import('./unsubscribe-rxjs/unsubscribe-rxjs.module').then(m => m.UnsubscribeRxjsModule)
+    //loadChildren: './unsubscribe-rxjs/unsubscribe-rxjs.module#UnsubscribeRxjsModule' 
+  }
 ];
 
 @NgModule({
