eb2f471ae79f5839466834027740ee835b3829d8
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-05-07 16:37:37 -0300

    Removing CORS using Angular Proxy

--- Angular-Requests-http/server/src/index.js
+++ Angular-Requests-http/server/src/index.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const bodyParser = require('body-parser');
 const multipart = require('connect-multiparty');
 
@@ -8,11 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+
+// const corsOptions = {
+//   origin: '*',
+//   optionsSuccessStatus: 200
+// };
+// app.use(cors(corsOptions));
 
 const multipartMiddleware = multipart({ uploadDir: './uploads' });
 app.post('/upload', multipartMiddleware, (req, res) => {
