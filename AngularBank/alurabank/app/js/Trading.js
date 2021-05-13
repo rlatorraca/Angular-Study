60ae266b79d16b2878f74f318f6058ea3a60005a
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-05-13 18:28:15 -0300

    tsc -w added

--- AngularBank/alurabank/app/js/Trading.js
+++ AngularBank/alurabank/app/js/Trading.js
@@ -1,31 +1,20 @@
 class Trading {
-    
-    private _date: Date;
-    private _quantity: number;
-    private _value: number;
-
     constructor(date, quantity, value) {
         // _ : diz que o atributo é PRIVATE
         this._date = date;
         this._quantity = quantity;
         this._value = value;
     }
-
-
     get date() {
         return this._date;
     }
-
     get quantity() {
         return this._quantity;
     }
-
     get value() {
         return this._value;
     }
-
     get volume() {
-
         return this._quantity * this._value;
     }
 }
