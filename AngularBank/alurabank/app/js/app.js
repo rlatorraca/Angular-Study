2a31a204ab0d1e9234b4893259254eb9150142a1
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-05-15 12:55:19 -0300

    View added

--- AngularBank/alurabank/app/js/app.js
+++ AngularBank/alurabank/app/js/app.js
@@ -1,4 +1,4 @@
 const controller = new TradingController();
 document
     .querySelector('form')
-    .addEventListener('submit', controller.adicionar.bind(controller));
+    .addEventListener('submit', controller.add.bind(controller));
