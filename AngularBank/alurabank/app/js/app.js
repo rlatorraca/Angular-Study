f0d6131c3c67215d639f2f7445044707e55a0725
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-05-17 10:02:32 -0300

    Added loader "system"

--- AngularBank/alurabank/app/js/app.js
+++ AngularBank/alurabank/app/js/app.js
@@ -1,3 +1,16 @@
-import { TradingController } from "./controllers/TradingController";
-const controller = new TradingController();
-$('.form').submit(controller.add.bind(controller));
+System.register(["./controllers/TradingController"], function (exports_1, context_1) {
+    "use strict";
+    var TradingController_1, controller;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (TradingController_1_1) {
+                TradingController_1 = TradingController_1_1;
+            }
+        ],
+        execute: function () {
+            controller = new TradingController_1.TradingController();
+            $('.form').submit(controller.add.bind(controller));
+        }
+    };
+});
