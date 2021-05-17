6accd3625acc9b97736f3940cf010ac05c39f43c
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-05-17 01:01:18 -0300

    export and import module on TS

--- AngularBank/alurabank/app/ts/views/TradesView.ts
+++ AngularBank/alurabank/app/ts/views/TradesView.ts
@@ -1,35 +1,35 @@
-namespace Views {
+import { Trades } from "../models/Trades";
+import { View } from "./View";
 
-    export class TradesView extends Views.View<Trades> {
+export class TradesView extends View<Trades> {
 
+    template(model: Trades): string {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATE</th>
+                    <th>QUANTITY</th>
+                    <th>VALUE</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
 
-        template(model: Trades): string {
-            return `
-            <table class="table table-hover table-bordered">
-                <thead>
+            <tbody>
+                ${model.toArray().map(trade => {
+            return `   
                     <tr>
-                        <th>DATE</th>
-                        <th>QUANTITY</th>
-                        <th>VALUE</th>
-                        <th>VOLUME</th>
-                    </tr>
-                </thead>
-    
-                <tbody>
-                    ${model.toArray().map(trade => {
-                return `   
-                        <tr>
-                            <td>${trade.date.getDate()}/${trade.date.getMonth() + 1}/${trade.date.getFullYear()}</td>
-                            <td>${trade.quantity}</td>
-                            <td>${trade.value}</td>
-                            <td>${trade.volume}</td>
-                        </tr>                        
-                    `}).join('')}   
-                </tbody>
-                   
-                <tfoot>
-                </tfoot>
-             </table> `;
-        }
+                        <td>${trade.date.getDate()}/${trade.date.getMonth() + 1}/${trade.date.getFullYear()}</td>
+                        <td>${trade.quantity}</td>
+                        <td>${trade.value}</td>
+                        <td>${trade.volume}</td>
+                    </tr>                        
+                `}).join('')}   
+            </tbody>
+                
+            <tfoot>
+            </tfoot>
+            </table> `;
     }
+
 }
