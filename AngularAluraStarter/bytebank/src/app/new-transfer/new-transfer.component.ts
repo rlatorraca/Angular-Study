b1680a7a8eb5e1ab8a9af2e58de3148748f672b7
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-07-14 01:20:50 -0300

    First steps

---
+++ AngularAluraStarter/bytebank/src/app/new-transfer/new-transfer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-transfer',
+  templateUrl: './new-transfer.component.html',
+  styleUrls: ['./new-transfer.component.scss']
+})
+export class NewTransferComponent {
+
+
+  public transfer(): any {
+    console.log('Transfering');
+  }
+
+}
