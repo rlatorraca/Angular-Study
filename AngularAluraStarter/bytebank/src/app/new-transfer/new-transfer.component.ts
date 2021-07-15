f82a8ac544f2afb705703a00c8806e2d2d377715
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-07-14 23:15:30 -0300

    Property Bind added on Form

--- AngularAluraStarter/bytebank/src/app/new-transfer/new-transfer.component.ts
+++ AngularAluraStarter/bytebank/src/app/new-transfer/new-transfer.component.ts
@@ -7,9 +7,13 @@ import { Component } from '@angular/core';
 })
 export class NewTransferComponent {
 
+  valor = 0 ;
+  destino = 0 ;
 
   public transfer(): any {
     console.log('Transfering');
+    console.log(this.valor);
+    console.log(this.destino);
   }
 
 }
