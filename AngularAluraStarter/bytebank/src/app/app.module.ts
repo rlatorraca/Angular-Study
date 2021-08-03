af642552f881d6680586075dafe619d90f610fb8
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-08-03 00:14:34 -0300

    Add dynamic table on show-transfer

--- AngularAluraStarter/bytebank/src/app/app.module.ts
+++ AngularAluraStarter/bytebank/src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NewTransferComponent } from './new-transfer/new-transfer.component';
 import { ShowTransferComponent } from './show-transfer/show-transfer.component';
+import localeCAFr from '@angular/common/locales/fr-CA';
+
+registerLocaleData( localeCAFr, 'fr-CA' );
 
 @NgModule({
   declarations: [
@@ -17,7 +21,10 @@ import { ShowTransferComponent } from './show-transfer/show-transfer.component';
     FormsModule
 
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-CA'},
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'CA$'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
